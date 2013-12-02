9a4f0c97d8bcb4853892ba172270c5ba006d7003
AntoineBouquet <bouquetant@gmail.com>
2013-12-02 11:54:02 +0100

    ToDolist version Alpha 0.1 02/12 matin

--- src/com/todolist/MainActivity.java
+++ src/com/todolist/MainActivity.java
@@ -3,16 +3,20 @@ package com.todolist;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.*;
 
 public class MainActivity extends Activity {
 	Button b = null;
+	ImageView image = null;
 	EditText entreeText = null;
 	TextView listeChoseAFaire = null;
-	String ChoseAFaire = "\tRien";
+	String choseAFaire = null;
 	int compteur = 0;
+	Animation anim = null;
 	  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +28,23 @@ public class MainActivity extends Activity {
 	    
 	    //Liste des choses à faire
 	    listeChoseAFaire = (TextView) findViewById(R.id.ChoseAFaire);
-	    listeChoseAFaire.setText(ChoseAFaire);
 	    
 	    //Gestion d'évènements du bouton ajouter
 	    b = (Button) findViewById(R.id.bouton);
 	    b.setOnClickListener(envoyerListener);
+	    
+	    //Initialisation de croix_rouge.png
+	    image = (ImageView) findViewById(R.id.imageSuppression);
+	   
+	    //Initialisation de la liste vide avec la croix effacée
+	    if(compteur == 0){
+	    	choseAFaire = "\n\tRien";
+	    	image.setVisibility(image.GONE);
+	    }
+	    listeChoseAFaire.setText(choseAFaire);
+	    
+	    //Initialisation de l'animation
+	    anim = AnimationUtils.loadAnimation(getApplication(), R.anim.tanslate);
 	}
 	
 	
@@ -36,19 +52,25 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onClick(View v) {
+			
 			switch(v.getId()) {
 			    case R.id.bouton:
 			    	if(entreeText.getText().toString().length() > 0){
 				    	if(compteur == 0){
-				    		ChoseAFaire = "\t- " + entreeText.getText().toString();
+				    		choseAFaire = "\n\t- " + entreeText.getText().toString();
 				    		compteur++;
+				    		image.setVisibility(image.VISIBLE);
 				    	}
 				    	else
-				    		ChoseAFaire = ChoseAFaire + "\n\t- " + entreeText.getText().toString();
-				    		listeChoseAFaire.setText(ChoseAFaire);
-				    		entreeText.setText(null);
+				    		choseAFaire = choseAFaire + "\n\t- " + entreeText.getText().toString();
+				    	
+				    	listeChoseAFaire.setText(choseAFaire);
+				    	entreeText.setText(null);
+				    	listeChoseAFaire.startAnimation(anim);
+				    	image.startAnimation(anim);
 			    	}
 			    	break;
+			     
 		    
 			}
 			
