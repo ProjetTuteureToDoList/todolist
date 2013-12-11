006e4542f4c731226fd45b23a0f15c69290bc203
AntoineBouquet <bouquetant@gmail.com>
2013-12-11 11:02:28 +0100

    Test des évenements de la croix "supprimer" fonctionnel !!!

--- src/com/Activites/ListeTacheAdapter.java
+++ src/com/Activites/ListeTacheAdapter.java
@@ -1,10 +1,13 @@
 package com.Activites;
 
+import java.util.ArrayList;
+
 import gestionDesTaches.ListeTaches;
 import gestionDesTaches.Tache;
 
 import com.todolist.R;
 
+import android.view.View.OnClickListener;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -55,31 +59,48 @@ public class ListeTacheAdapter extends BaseAdapter{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LinearLayout layout = null;
-		
 		// Si la vue est recyclée, il contient déjà le bon layout, donc pas besoin de la recréer
 		// pour des soucis d'optimisation du processus
 		if(convertView != null)
 			// On n'a plus qu'à le récupérer
-			layout  = (LinearLayout) convertView;
+			layout = (LinearLayout) convertView;
 		else{
 			// Sinon, il faut utiliser le LayoutInflater
+			
 			layout = (LinearLayout) mInflater.inflate(R.layout.tache_page_principale, parent, false);
 			
-			//ANIMATION
+			//ANIMATION SUR AJOUT, MAGNIFIQUE !!!!!!
 			if(position == lt.getTabTache().size() - 1 && !lt.getTabTache().get(position).getAnimation()){
 				Animation anim = AnimationUtils.loadAnimation(this.listeContexte, R.anim.tanslate);
 				layout.startAnimation(anim);
 				lt.getTabTache().get(position).setAnimation(true);
 			}
 		}
-		 
-		TextView laTache = (TextView) layout.findViewById(R.id.tache);
-		laTache.setText(lt.getTabTache().get(position).getNom());
-
+		
+		TextView tacheNom = (TextView) layout.findViewById(R.id.tache);
+		ImageView imageSuppr = (ImageView) layout.findViewById(R.id.croix_suppr);
+		
+		
+		tacheNom.setText(lt.getTabTache().get(position).getNom());
+		imageSuppr.setTag(position);
+		
+		imageSuppr.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//Lorsque l'on clique sur la croix, on récupère la position de la Tache
+				Integer position = (Integer)v.getTag();
+						
+				//On prévient les listeners qu'il y a eu un clic sur l'ImageView "mCroixSuppr".
+				sendListener(position);
+						
+			}
+		        	
+		});
 		
 		return layout;	
 	}
-
+	
 	/** retourne la taille de l'adapter
 	 * c'est à dire la taille de la liste de tâche
 	 */
@@ -89,6 +110,7 @@ public class ListeTacheAdapter extends BaseAdapter{
 		return this.lt.getTabTache().size();
 	}
 	
+	
 	public void ajoutTacheAdapter(String nouvelleTache){
 		lt.ajoutTache(new Tache(nouvelleTache));
 	}
@@ -96,4 +118,25 @@ public class ListeTacheAdapter extends BaseAdapter{
 	public void suppressionTacheAdapter(int position){
 		lt.suppressionTache(position);
 	}
+	
+////////INTERFACE LISTENER POUR LA CROIX
+	public interface CroixAdapterListener {
+	    public void onClickCroix(int position);
+	}
+	
+	private ArrayList<CroixAdapterListener> mListListener = new ArrayList<CroixAdapterListener>();
+	/**
+	 * Pour ajouter un listener sur notre adapter
+	 */
+	public void addListener(CroixAdapterListener aListener) {
+	    mListListener.add(aListener);
+	}
+
+	private void sendListener(int position) {
+	    for(int i = mListListener.size()-1; i >= 0; i--) {
+	    	mListListener.get(i).onClickCroix(position);
+	    }
+	}
+/////////
+	
 }
--- src/com/Activites/MainActivity.java
+++ src/com/Activites/MainActivity.java
@@ -1,17 +1,20 @@
 package com.Activites;
 
 import gestionDesTaches.Tache;
-
-import com.todolist.R;
-
+import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.os.Bundle;
-import android.app.Activity;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+import com.Activites.ListeTacheAdapter.CroixAdapterListener;
+import com.todolist.R;
+
+public class MainActivity extends Activity implements CroixAdapterListener {
 	Button b = null;
 	EditText entreeText = null;
 	ListeTacheAdapter lta = null;
@@ -34,13 +37,13 @@ public class MainActivity extends Activity {
 	    lta = new ListeTacheAdapter(this);
 	    checkList = (ListView) findViewById(R.id.listview);
 	    checkList.setAdapter(lta);
-	    checkList.setOnItemLongClickListener(tacheLongListener);
-	    checkList.setOnItemClickListener(tacheListener);
-	    
+	    checkList.setOnItemLongClickListener(tacheLongListener);	    
 	    
 	    //Initialisation et gestion de l'évènement clic du bouton ajouter
 	    b = (Button) findViewById(R.id.bouton);
 	    b.setOnClickListener(ajouterListener);
+	    
+	    lta.addListener(this);
 	    
 	}
 	
@@ -71,8 +74,9 @@ public class MainActivity extends Activity {
 	
 	private AdapterView.OnItemClickListener tacheListener = new AdapterView.OnItemClickListener() {
 		@Override
-        public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
-			lta.suppressionTacheAdapter(position);
+        public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+			temp = position;
+			checkList.getChildAt(position).setOnClickListener(ajouterListener);
 			actualiserListe();
         }
 	};
@@ -80,7 +84,6 @@ public class MainActivity extends Activity {
 	private AdapterView.OnItemLongClickListener tacheLongListener = new AdapterView.OnItemLongClickListener() {
 		@Override
         public boolean onItemLongClick(AdapterView<?> arg0, View v, int position, long arg3) {
-			temp = position;
             lta.ajoutTacheAdapter(((Tache) lta.getItem(position)).getNom());
 	    	actualiserListe();
 			return true;
@@ -96,5 +99,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClickCroix(int position) {
+		lta.suppressionTacheAdapter(position);
+		actualiserListe();
 	}
 }
