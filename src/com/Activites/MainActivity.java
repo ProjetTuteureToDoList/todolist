ed9130afee996d6a54d3d9624fac751fc0081612
AntoineBouquet <bouquetant@gmail.com>
2013-12-17 15:20:37 +0100

    Rajout d'un bandeau bleu qui contiendra les icônes Retour, Paramètres, Menu, etc (+ rajout de la police Lifestyle trouvé sur Dafont, pour le titre ToDoList) Bug du vidage de ListView enlevé : désactivation de la rotation de l'écran Focus du clavier à l'ouverture enlevé + rajout d'évènement enlevant ce même focus lors d'un clic sur l'écran

--- src/com/Activites/MainActivity.java
+++ src/com/Activites/MainActivity.java
@@ -2,33 +2,47 @@ package com.Activites;
 
 import gestionDesTaches.Tache;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.Activites.ListeTacheAdapter.CroixAdapterListener;
 import com.todolist.R;
 
 public class MainActivity extends Activity implements CroixAdapterListener {
-	Button b = null;
-	EditText entreeText = null;
-	ListeTacheAdapter lta = null;
+	Button boutonAjouter = null;		//la variable du bouton	"Ajouter !"		
+	EditText entreeText = null;			//enntrée de texte pour le rajout de tâche
+	ListeTacheAdapter lta = null;		//représente la listView adapté pour la ListeTache
+	LinearLayout allScreen = null;		//représente le layout englobant tout l'écran
 	
 	/*Permet de contrôler la ListView d'activity_main contenant
 	 * toutes les vues de tache_page_principale.xml
 	 */
-	ListView checkList = null;			
+	ListView checkList = null;			//Permet de faire le lien entre la lta et une ListView donné dans activity_main.xml
 	  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Temporaire : masquage du TextView "Retour"
+		TextView retour = (TextView) findViewById(R.id.retour);
+		retour.setVisibility(8);
+		
+		//Initialisation du titre : ouverture du fichier pour la police 
+		TextView texteView = (TextView) findViewById(R.id.titre);
+		Typeface font = Typeface.createFromAsset(getAssets(), "Lifestyle M54.ttf");
+		texteView.setTypeface(font);
 		
 		//Initialisation EditText pour la gestion les évenèments
 	    entreeText = (EditText) findViewById(R.id.entreeTexte);
@@ -41,12 +55,12 @@ public class MainActivity extends Activity implements CroixAdapterListener {
 	    checkList.setOnItemLongClickListener(tacheLongListener);
 	    
 	    //Initialisation d'un LinearLayout représentant la totalité de l'écran
-	    LinearLayout t = (LinearLayout) findViewById(R.id.allScreen);
-	    t.setOnClickListener(ajouterListener);
+	    allScreen = (LinearLayout) findViewById(R.id.allScreen);
+	    allScreen.setOnClickListener(ajouterListener);
 	    
 	    //Initialisation et gestion de l'évènement clic du bouton ajouter
-	    b = (Button) findViewById(R.id.bouton);
-	    b.setOnClickListener(ajouterListener);
+	    boutonAjouter = (Button) findViewById(R.id.bouton);
+	    boutonAjouter.setOnClickListener(ajouterListener);
 	    
 	    //Initialisation du Listener de la Croix
 	    lta.addListener(this);
@@ -64,11 +78,15 @@ public class MainActivity extends Activity implements CroixAdapterListener {
 			    	if(entreeText.getText().toString().length() > 0){
 			    		lta.ajoutTacheAdapter(entreeText.getText().toString());
 				    	entreeText.setText(null);
+				    	entreeText.clearFocus();
+				    	InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+				    	inputMethodManager .hideSoftInputFromWindow(entreeText.getWindowToken(), 0);
 			    	}
 			    	break;
 			    case R.id.allScreen:
 			    		for(int i = 0 ; i < lta.getCount() ; i++)
 			    			lta.setOptionTacheVisibilite(false, i);
+			    		entreeText.clearFocus();
 		    
 			}
 			
