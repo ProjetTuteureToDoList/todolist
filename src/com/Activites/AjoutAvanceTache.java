020361f316b874ff96990d2791380ae21fc67439
Julien <j.lithyum@gmail.com>
2014-01-16 21:20:11 +0100

    début d'implémentation de la tache avancé en java

--- src/com/Activites/AjoutAvanceTache.java
+++ src/com/Activites/AjoutAvanceTache.java
@@ -3,11 +3,17 @@ package com.Activites;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.todolist.R;
 
 public class AjoutAvanceTache extends Activity{
+	String nomDeTache = (((EditText) findViewById(R.id.nomDeTache)).getText()).toString();
+	String details = (((EditText) findViewById(R.id.detailTache)).getText()).toString();
+	int importance = Integer.parseInt((((EditText) findViewById(R.id.importanceTacheText)).getText()).toString());
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +25,5 @@ public class AjoutAvanceTache extends Activity{
 		Typeface font = Typeface.createFromAsset(getAssets(), "Lifestyle M54.ttf");
 		titre.setTypeface(font);
 	}
+	
 }
