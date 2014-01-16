e71c628371ff3ff4b2758b328b265abea17004f6
MattStengel <matthieustengel@gmail.com>
2014-01-16 17:03:54 +0100

    Implémentation des boutons de l'activité TâcheDescriptif (non fonctionnels pour le moment , provoque des forces closes pour l'ouverture de la description d'une tâche)

--- src/com/Activites/DescriptifTache.java
+++ src/com/Activites/DescriptifTache.java
@@ -1,14 +1,26 @@
 package com.Activites;
 
+import Adapters.ListeTacheAdapter;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.todolist.R;
 
 public class DescriptifTache extends Activity{
 	
+	ImageView retour = null;
+	ImageView modification = null;
+	ImageView suppr = null;
+	ListeTacheAdapter lta = null;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,20 +31,95 @@ public class DescriptifTache extends Activity{
 		Typeface font = Typeface.createFromAsset(getAssets(), "Lifestyle M54.ttf");
 		titre.setTypeface(font);
 		
+		retour = (ImageView) findViewById(R.id.retour);
+		modification = (ImageView) findViewById(R.id.modification);
+		suppr = (ImageView) findViewById(R.id.corbeille);
+		
+		retour.setOnTouchListener(touchClick);
+		modification.setOnTouchListener(touchClick);
+		suppr.setOnTouchListener(touchClick);
+		
 		TextView tache = (TextView) findViewById(R.id.tache);
-		tache.setText("La tâche s'appelle " + getIntent().getStringExtra("nom") + 
-					  "\nSa description est : " + getIntent().getStringExtra("description") +
-					  "\nDate : " + getIntent().getIntExtra("dateJour", 1) + 
+		tache.setText("Nom: " + getIntent().getStringExtra("nom") + 
+					  "\nDescription : " + getIntent().getStringExtra("description") +
+					  "\nDate : " + getIntent().getIntExtra("date", 1) + 
 					  "/" + getIntent().getIntExtra("dateMois", 1) + 
-					  "/" + getIntent().getIntExtra("dateAnnee", 1970) + 
-					  "\nImportance de la tâche : " + getIntent().getIntExtra("importance", 1) + 
+					  "/" + getIntent().getIntExtra("dateAnnee", 1970) +
+					  "\nPriorité : " + getIntent().getIntExtra("importance", 1) + 
 					  "\nEtat : ");
 		if(getIntent().getBooleanExtra("etat", true))
 			tache.setText(tache.getText() + "Fait");
 		else
 			tache.setText(tache.getText() + "Non fait");
 		
-		
 	}
 	
+	private View.OnTouchListener touchClick = new OnTouchListener(){
+		boolean isInside = true;
+		float viewLargeur = 0;
+		float viewHauteur = 0;
+	
+	
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		
+		if(v.getId() == R.id.corbeille || v.getId() == R.id.retour ||v.getId() == R.id.modification){
+			switch(event.getAction()){
+				case MotionEvent.ACTION_DOWN:
+					viewLargeur = v.getRight() - v.getLeft();
+					viewHauteur = v.getBottom() - v.getTop();
+					v.setBackgroundResource(R.color.bleuSelection);
+					break;
+				case MotionEvent.ACTION_UP:
+					v.setBackgroundResource(R.color.bleu);
+					if(isInside){
+						switch(v.getId()){
+								
+							case R.id.corbeille:
+							
+								Builder confirmationSuppr = new AlertDialog.Builder(DescriptifTache.this);
+									confirmationSuppr.setTitle("Êtes-vous sûr de supprimer cette tâche ?");
+							
+								confirmationSuppr.setPositiveButton("Oui", new DialogInterface.OnClickListener(){
+									public void onClick(DialogInterface dialog, int id){
+								    		if(lta.getItem(id).getAfficheOption()){
+								    			lta.suppressionTacheAdapter(id);
+								    		}
+								    	}
+									
+									}
+								);
+								
+								confirmationSuppr.setNegativeButton("Non", new DialogInterface.OnClickListener(){
+									public void onClick(DialogInterface dialog, int id){
+									}
+								});
+								confirmationSuppr.show();
+						    	break;
+						    	
+							case R.id.retour:
+						    	break;	
+						    
+							case R.id.icone_tags:
+								break;
+						}
+					}
+					break;
+					
+				case MotionEvent.ACTION_MOVE:
+					if(event.getX() < viewLargeur && event.getY() < viewHauteur && event.getX() > 0 && event.getY() > 0)
+						isInside = true;
+					else
+						isInside = false;
+					break;
+			}
+			
+			return true;
+		}
+		else
+			return false;
+	}
+	
+};
+
 }
