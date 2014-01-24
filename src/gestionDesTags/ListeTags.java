dfe1066b3d2c8f7156959c07d0b7b4f230a7ead5
AntoineBouquet <bouquetant@gmail.com>
2014-01-24 11:19:47 +0100

    Rajout du fichier ListeTags.java

---
+++ src/gestionDesTags/ListeTags.java
@@ -0,0 +1,9 @@
+package gestionDesTags;
+
+import java.util.ArrayList;
+
+public class ListeTags {
+	private ArrayList<Tag> tabTache = new ArrayList<Tag>();
+	private int compteurTag;
+	private BDDTag db;
+}
